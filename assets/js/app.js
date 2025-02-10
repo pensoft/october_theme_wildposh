43580710655dc9ba8f7a62979f6889aa752138e7
Teodor <preprint@pensoft.net>
2025-02-10 11:43:47 +0200

    prod changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -232,7 +232,7 @@ $(document).ready(function() {
     
     
 
-    $('.events .tabs').each(function(){
+    $('.events .tabs, .media-center .tabs').each(function(){
         // For each set of tabs, we want to keep track of
         // which tab is active and its associated content
         var $active, $content, $links = $(this).find('a');
